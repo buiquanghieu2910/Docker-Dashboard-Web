bba70b4505ff21e2e5ded4b6bc3c57faa4eacb2e
buiquanghieu2910 <bui.quang.hieu2910@gmail.com>
2024-01-16 20:36:32 +0700

    commit

--- client/src/components/containers/AllData.vue
+++ client/src/components/containers/AllData.vue
@@ -1,31 +1,63 @@
 <template>
   <div>
     <el-table :data="data" style="width: 100%">
-      <el-table-column prop="status" label="Status">
+      <el-table-column prop="status" label="Status" width="150">
         <template #default="scope">
           <el-switch v-model="scope.row.state.running"
         /></template>
       </el-table-column>
-      <el-table-column prop="name" label="Name" />
-      <el-table-column prop="short_id" label="ID" />
-      <el-table-column label="Created At">
+
+      <el-table-column prop="name" label="Name" width="300"/>
+      
+      <el-table-column prop="short_id" label="ID" width="200"/>
+
+      <el-table-column label="Created At" width="200">
         <template #default="scope">
-          {{ formatTimeAgo(scope.row.created) }}
+          <el-tag class="mx-1" effect="dark" round type="success">
+            {{ formatTimeAgo(scope.row.created) }}
+          </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="CPU" v-if="status != 'stopped'">
+
+      <el-table-column label="CPU" v-if="status != 'stopped'" width="200">
         <template #default="scope">
-          {{ scope.row.stats.cpu_percentage }}
+          <el-tag
+            v-if="scope.row.stats.cpu_percentage"
+            class="mx-1"
+            effect="dark"
+            round
+            type="danger"
+          >
+            {{ scope.row.stats.cpu_percentage }}
+          </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="RAM" v-if="status != 'stopped'">
+
+      <el-table-column label="RAM" v-if="status != 'stopped'" width="200">
         <template #default="scope">
-          {{ scope.row.stats.memory_usage }}
+          <el-tag
+            v-if="scope.row.stats.memory_usage"
+            class="mx-1"
+            effect="dark"
+            round
+            type="info"
+          >
+            {{ scope.row.stats.memory_usage }}
+          </el-tag>
         </template></el-table-column
       >
-      <el-table-column label="Network" v-if="status != 'stopped'">
+
+      <el-table-column label="Network" v-if="status != 'stopped'" width="200">
         <template #default="scope">
-          {{ scope.row.stats.network_io }}
+          <el-tag
+            v-if="scope.row.stats.network_io"
+            class="mx-1"
+            effect="dark"
+            round
+            type="warning"
+          >
+            {{ scope.row.stats.network_io }}
+          </el-tag>
         </template>
       </el-table-column>
     </el-table>
